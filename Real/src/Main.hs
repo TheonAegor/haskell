510098afa8b8f247ed939cbc7a97dc05c3e7c84b
taegor <egorproskurin.95@gmail.com>
2020-09-28 17:12:50 +0300

    partial applicatin v2.0

--- Real/src/Main.hs
+++ Real/src/Main.hs
@@ -1,39 +1,16 @@
---добавляет псевдоним, вместо String можно использовать Login 
-type Login = String
-type Password = String
-type AvatarURL = String
 type UserId = Integer
+type Prefix = String
 
--- '++' символ конкатенции двух строк
+obtainLogin :: UserId -> (Prefix -> String)
+obtainLogin userId = 
+    loginStorage "denis"
+    where loginStorage login prefix = prefix ++ ": " ++ login
+--Здесь мы, на основании полученного извне идентификатора пользователя,
+--откуда-то извлекаем логин и сразу же прячем его в λ-функцию, тут же нами и созданную. В результате функция obtainLogin возвращает частично
+--применённую функцию, которую мы вторично применяем к строке-пре­
+--фиксу — и в результате на выходе мы получаем готовый результат:
 
-userInfo :: Login -> Password -> AvatarURL -> UserId -> String
-userInfo login password avatarURL userId =
-        "Full info about user @" ++ (show userId) ++ ":" ++
-        "\n login: " ++ login ++
-        "\n password: " ++ password ++
-        "\n avatar URL: " ++ avatarURL
-
-type EmptyInfo = Login -> Password -> AvatarURL -> UserId -> String
-type WithLogin =          Password -> AvatarURL -> UserId -> String
-type AndWithPassword =                AvatarURL -> UserId -> String
-type AndWithAvatarURL =                            UserId -> String
-
-storeLoginIn :: EmptyInfo -> UserId -> WithLogin
-storeLoginIn emptyInfo userId = 
-        emptyInfo "denis"
-
-storePasswordIn :: WithLogin -> UserId -> AndWithPassword
-storePasswordIn infoWithLogin userId = infoWithLogin "123456789abc"
-
-storeAvatarURLIn :: AndWithPassword -> UserId -> AndWithAvatarURL
-storeAvatarURLIn infoWithPassword userId =
-        infoWithPassword "http://dshevchenko.biz/denis_avatar.png"
-
-main = 
-        let userId = 1234
-            infoWithLogin = storeLoginIn userInfo userId
-            infoWithPassword = storePasswordIn infoWithLogin userId
-            infoWithAvatarURL = storeAvatarURLIn infoWithPassword userId
-            fullInfoAboutUser = infoWithAvatarURL userId
-        in
-        putStrLn fullInfoAboutUser
+main =
+    let userId = 1234
+    in
+    putStrLn ((obtainLogin userId) "My login")
