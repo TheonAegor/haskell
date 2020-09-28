84820a490a4cd0ef33b29696a25efc19b564eaff
taegor <egorproskurin.95@gmail.com>
2020-09-28 17:01:56 +0300

    partial application v1.0

--- Real/src/Main.hs
+++ Real/src/Main.hs
@@ -1,4 +1,39 @@
+--добавляет псевдоним, вместо String можно использовать Login 
+type Login = String
+type Password = String
+type AvatarURL = String
+type UserId = Integer
 
+-- '++' символ конкатенции двух строк
 
-main = print (f 5 6)
-        where f = \x y -> x * y 
+userInfo :: Login -> Password -> AvatarURL -> UserId -> String
+userInfo login password avatarURL userId =
+        "Full info about user @" ++ (show userId) ++ ":" ++
+        "\n login: " ++ login ++
+        "\n password: " ++ password ++
+        "\n avatar URL: " ++ avatarURL
+
+type EmptyInfo = Login -> Password -> AvatarURL -> UserId -> String
+type WithLogin =          Password -> AvatarURL -> UserId -> String
+type AndWithPassword =                AvatarURL -> UserId -> String
+type AndWithAvatarURL =                            UserId -> String
+
+storeLoginIn :: EmptyInfo -> UserId -> WithLogin
+storeLoginIn emptyInfo userId = 
+        emptyInfo "denis"
+
+storePasswordIn :: WithLogin -> UserId -> AndWithPassword
+storePasswordIn infoWithLogin userId = infoWithLogin "123456789abc"
+
+storeAvatarURLIn :: AndWithPassword -> UserId -> AndWithAvatarURL
+storeAvatarURLIn infoWithPassword userId =
+        infoWithPassword "http://dshevchenko.biz/denis_avatar.png"
+
+main = 
+        let userId = 1234
+            infoWithLogin = storeLoginIn userInfo userId
+            infoWithPassword = storePasswordIn infoWithLogin userId
+            infoWithAvatarURL = storeAvatarURLIn infoWithPassword userId
+            fullInfoAboutUser = infoWithAvatarURL userId
+        in
+        putStrLn fullInfoAboutUser
