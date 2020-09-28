247ebed67da094a70cda37d08c9ab84a71f6e0e0
taegor <egorproskurin.95@gmail.com>
2020-09-28 15:17:08 +0300

    lambda functions

--- Real/src/Main.hs
+++ Real/src/Main.hs
@@ -1,16 +1,4 @@
-prepareLength :: Double -> Double
-prepareLength line = 
-                    line * coefficient * correlation
-                    where coefficient = 0.4595
-                          correlation = 1.2
 
-changeNum :: Double -> Double
-changeNum nbr = 
-                let coefficient = 3.14
-                    correction = 0.015
-                in
-                nbr * coefficient / correction
 
-noNameFunc x = x * x
-
-main = print ( prepareLength (changeNum (noNameFunc 4)))
+main = print (f 5 6)
+        where f = \x y -> x * y 
