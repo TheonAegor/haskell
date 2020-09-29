157f53fd6338fbf3a038073c54be5a90c816d638
taegor <egorproskurin.95@gmail.com>
2020-09-29 10:47:55 +0300

    partial application v1.1

--- Real/src/Main.hs
+++ Real/src/Main.hs
@@ -1,16 +1,9 @@
-type UserId = Integer
-type Prefix = String
+divide :: Double -> Double -> Double
+divide x y = x / y 
 
-obtainLogin :: UserId -> (Prefix -> String)
-obtainLogin userId = 
-    loginStorage "denis"
-    where loginStorage login prefix = prefix ++ ": " ++ login
---Здесь мы, на основании полученного извне идентификатора пользователя,
---откуда-то извлекаем логин и сразу же прячем его в λ-функцию, тут же нами и созданную. В результате функция obtainLogin возвращает частично
---применённую функцию, которую мы вторично применяем к строке-пре­
---фиксу — и в результате на выходе мы получаем готовый результат:
-
-main =
-    let userId = 1234
+main = 
+    let tempFunc = divide 10.03
     in
-    putStrLn ((obtainLogin userId) "My login")
+    print (tempFunc 2.1)
+
+--let tempFunc = \x -> 10.03 / x; можно заменить let в main 
